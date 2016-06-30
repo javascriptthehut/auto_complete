23ea4d10e3bdc2987d7a97a4388786bbfd06a206
samgalson@gmail.com <samgalson@gmail.com>
2016-06-30 12:13:06 +0100

    added xhr req and handle response functions

--- public/interface.js
+++ public/interface.js
@@ -1,1 +1,31 @@
-console.log('loaded')
+document.getElementById('input').addEventListener("change", function() {
+
+});
+
+function xhr (handleResult) {
+    var xhr = new XMLHttpRequest()
+    xhr.onreadystatechange = function () {
+        if(xhr.readyState === 4 && xhr.status === 200) {
+          handleResult(xhr.response)
+        }
+    }
+    xhr.open('get', '/find/' + document.getElementById('input').value);
+    xhr.send();
+}
+
+
+function updateDom() {
+
+}
+
+function handleResult(input) {
+    var array = input.split(',');
+    var dropdown = document.getElementsByClassName('dropdown')[0]
+    array.forEach(function(text){
+      var p = document.createElement('p')
+      var text = document.createTextNode(text)
+      p.appendChild(text)
+      dropdown.appendChild(p)
+    })
+
+}
