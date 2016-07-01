309f3960875a2015216e79e0a3f90d84acd8690c
samgalson@gmail.com <samgalson@gmail.com>
2016-07-01 15:10:48 +0100

    ?

--- test/frontend-tests.js
+++ test/frontend-tests.js
@@ -16,13 +16,35 @@
 	});
 	describe('interface.js', function () {
 
-		it('does one thing',function(){
-
+		beforeEach(function() {
+			jasmine.Ajax.install();
 		});
 
-		it('does another thing',function(){
-
+		afterEach(function() {
+			jasmine.Ajax.uninstall();
 		});
-	})
+
+		it('sends xhr request',function(){
+			/*
+			   function handleResult(input) {
+			   var array = input.split(',');
+			   var dropdown = document.getElementsByClassName('dropdown')[0];
+			   removeChildren();
+			   array.forEach(function(text){
+			   var p = document.createElement('p');
+			   p.className = 'option';
+			   var text = document.createTextNode(text);
+			   p.appendChild(text);
+			   dropdown.appendChild(p);
+			   */
+			handleResult('one,two,three,four')
+		});
+
+	});
+
+	it('does another thing',function(){
+
+	});
+})
 })();
 
