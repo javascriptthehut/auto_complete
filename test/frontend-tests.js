fce1961474becb81aa71fe58bfb641edd9270425
samgalson@gmail.com <samgalson@gmail.com>
2016-07-01 12:43:21 +0100

    ?

--- test/frontend-tests.js
+++ test/frontend-tests.js
@@ -1,1 +1,24 @@
 //jasmine
+
+(function () {
+	'use strict';
+
+	describe('Testing apiCaller methods', function () {
+
+		describe('this tests are for the AJAX call', function () {
+			it('',function(){
+				var doneFn = jasmine.createSpy("success");			
+				currencyOBJ.init()
+				expect(currencyOBJ.dates).toEqual(['2016-06-15','2016-06-08','2016-06-01','2016-05-25'])
+			});
+			it('',function(){
+				currencyOBJ.init()
+				expect(currencyOBJ.dates).toEqual(['2016-06-15','2016-06-08','2016-06-01','2016-05-25'])
+			});
+
+
+
+		});
+	});
+})();
+
